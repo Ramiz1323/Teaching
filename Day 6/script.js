39f0dd10cdd531c1e804b8f83132ee131c7d5b74
Sk Ramiz Raza <reenabibi5678@gmail.com>
2026-03-19 23:47:36 +0530

    Add Day 6 files with React components and styling for box and circle

---
+++ Day 6/script.js
@@ -0,0 +1,29 @@
+/* 
+React -> UI
+ReactDOM -> React and DOM communication
+
+React.createElement(1,2,3)
+
+let h1 = React.createElement("h1", {id: "h1", class: "heading"}, "This is H1")
+let h2 = React.createElement("h2", {id: "h2"}, "This is H2")
+
+let div = React.createElement("div", {class: "container"}, [h1, h2])
+
+//Initialize ReactDOM
+let root = ReactDOM.createRoot(document.querySelector(".root"))
+
+root.render(div)
+
+function h1() {
+    return React.createElement("h1", {id: "h1", class: "heading"}, "This is H1");
+}
+
+let root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(h1())
+*/
+
+import App from "./App.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(App())
