d68dda5a639b380e0013a10112b727f3576e789b
Sk Ramiz Raza <reenabibi5678@gmail.com>
2026-03-29 01:07:40 +0530

    feat: update note model schema to include type definitions and add Task.md for backend revision tasks

---
+++ Day 10/Backend/Task.md
@@ -0,0 +1,11 @@
+Schema =>
+title = "Task 1"
+description = "Backend Revision.."
+status = "Pending", "Completed", "Rejected"
+
+create
+get
+get by id
+status change
+delete
+update
--- Day 10/Backend/src/models/note.model.js
+++ Day 10/Backend/src/models/note.model.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 const noteSchema =  mongoose.Schema({
-    title: String,
-    description: String
+    title: {
+        type: String,
+    },
+    description: {
+        type: String,
+    }
 })
 
 const noteModel = mongoose.model('Note', noteSchema);
